098f66368a8ca298950cb6a5665eaafe2cd4930a
DenisOgnev <denisognev@icloud.com>
2022-04-07 21:53:48 +0300

    changed to long long type, still have problems with big number

--- include/algorithms.hpp
+++ include/algorithms.hpp
@@ -7,7 +7,7 @@ namespace Algorithms
 {
     namespace HNF
     {
-        Eigen::MatrixXi HNF_full_row_rank(const Eigen::MatrixXi &B);
+        Eigen::Matrix<int64_t, Eigen::Dynamic, Eigen::Dynamic> HNF_full_row_rank(const Eigen::Matrix<int64_t, Eigen::Dynamic, Eigen::Dynamic> &B);
         Eigen::MatrixXd HNF(const Eigen::MatrixXd &B);
     }
     namespace CVP
@@ -15,7 +15,7 @@ namespace Algorithms
         Eigen::VectorXd greedy(const Eigen::MatrixXd &matrix, const Eigen::VectorXd &target);
         Eigen::VectorXd branch_and_bound(const Eigen::MatrixXd &matrix, const Eigen::VectorXd &target);
     }
-    Eigen::MatrixXd gram_schmidt(const Eigen::MatrixXi &matrix, bool delete_zero_rows = true);
+    Eigen::MatrixXd gram_schmidt(const Eigen::Matrix<int64_t, Eigen::Dynamic, Eigen::Dynamic> &matrix, bool delete_zero_rows = true);
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 int main()
 {
-	Eigen::MatrixXi mat = Utils::generate_random_matrix_with_full_row_rank(15, 15, 1, 10);
+	Eigen::Matrix<int64_t, Eigen::Dynamic, Eigen::Dynamic> mat = Utils::generate_random_matrix_with_full_row_rank(10, 10, 1, 10);
 	//mat << 1, 1, -1, 2;
 	//mat << 6, 10, 6, 9, 1, 6, 5, 9, 5;
 // 	mat << 7, 10,  4,  6,  2, 10, 4,  6,  6,  9,  5,  9,
@@ -16,15 +16,13 @@ int main()
 //  2,  3,  5,  6,  2,  4,
 //  9,  9,  6,  4,  5,  3,
 //  5,  9,  1,  2,  2,  4;
-	//std::cout << Utils::matrix_to_string(mat) << "\n";
-	// double start_time = omp_get_wtime();
-	// Eigen::MatrixXi HNF = Algorithms::HNF::HNF_full_row_rank(mat);
-	// double end_time = omp_get_wtime();
-	// std::cout << end_time - start_time << "\n";
-	unsigned long long b = 18446744073709551615;
-	std::cout << b << "\n";
-	// std::cout << mat << "\n\n";
-	// std::cout << HNF.transpose() << "\n\n";
+	std::cout << Utils::matrix_to_string(mat) << "\n";
+	double start_time = omp_get_wtime();
+	Eigen::Matrix<int64_t, Eigen::Dynamic, Eigen::Dynamic> HNF = Algorithms::HNF::HNF_full_row_rank(mat);
+	double end_time = omp_get_wtime();
+	std::cout << end_time - start_time << "\n";
+	std::cout << mat << "\n\n";
+	std::cout << HNF.transpose() << "\n\n";
 	//std::cout << Utils::det_by_gram_schmidt(mat) << "\n";
 
 	// Eigen::MatrixXd mat = Utils::generate_random_matrix_with_full_row_rank(3, 3, 1, 5);
