037f30a90dca3d8da8daf6f49021ac79a514de8c
DenisOgnev <denisognev@icloud.com>
2022-04-25 10:23:03 +0300

    123

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -133,31 +133,65 @@ namespace Algorithms
     }
     namespace CVP
     {
-        bool first_time = true;
+        // bool first_time = true;
+        // Eigen::MatrixXd gram_schmidt;
+        // int index;
+        // Eigen::VectorXd greedy(const Eigen::MatrixXd &matrix, const Eigen::VectorXd &target)
+        // {
+        //     if (matrix.rows() == 0)
+        //     {
+        //         first_time = true;
+        //         return Eigen::VectorXd::Zero(matrix.cols());
+        //     }
+        //     if (first_time)
+        //     {
+        //         first_time = false;
+        //         gram_schmidt = Algorithms::gram_schmidt(matrix, false);
+        //         index = matrix.cols() - 1;
+        //     }
+        //     Eigen::VectorXd b = matrix.row(matrix.rows() - 1);
+        //     Eigen::MatrixXd B = matrix.block(0, 0, matrix.rows() - 1, matrix.cols());
+        //     //Eigen::VectorXd b_star = Utils::projection(B, b);
+        //     Eigen::VectorXd b_star = gram_schmidt.col(index);
+        //     index--;
+        //     double x = target.dot(b_star) / b_star.dot(b_star);
+        //     double c = round(x);
+
+        //     return c * b + Algorithms::CVP::greedy(B, target - c * b);
+        // }
         Eigen::MatrixXd gram_schmidt;
+        Eigen::MatrixXd B;
         int index;
-        Eigen::VectorXd greedy(const Eigen::MatrixXd &matrix, const Eigen::VectorXd &target)
+
+        Eigen::VectorXd greedy_recursive(const Eigen::VectorXd &target)
         {
-            if (matrix.rows() == 0)
+            if (index == 0)
             {
-                first_time = true;
-                return Eigen::VectorXd::Zero(matrix.cols());
+                return Eigen::VectorXd::Zero(target.rows());
             }
-            if (first_time)
-            {
-                first_time = false;
-                gram_schmidt = Algorithms::gram_schmidt(matrix, false);
-                index = matrix.cols() - 1;
-            }
-            Eigen::VectorXd b = matrix.row(matrix.rows() - 1);
-            Eigen::MatrixXd B = matrix.block(0, 0, matrix.rows() - 1, matrix.cols());
-            //Eigen::VectorXd b_star = Utils::projection(B, b);
-            Eigen::VectorXd b_star = gram_schmidt.col(index);
             index--;
+            Eigen::VectorXd b = B.col(index);
+            Eigen::VectorXd b_star = gram_schmidt.col(index);
             double x = target.dot(b_star) / b_star.dot(b_star);
+            if (b_star.isZero(1e-3))
+            {
+                b_star = b;
+                x = target.dot(b_star);
+            }
             double c = round(x);
 
-            return c * b + Algorithms::CVP::greedy(B, target - c * b);
+            return c * b + Algorithms::CVP::greedy_recursive(target - c * b);
+        }
+
+        Eigen::VectorXd greedy(const Eigen::MatrixXd &matrix, const Eigen::VectorXd &target)
+        {
+            B = matrix;
+            gram_schmidt = Algorithms::gram_schmidt(matrix, false);
+            index = static_cast<int>(matrix.cols());
+            
+            Eigen::VectorXd result = greedy_recursive(target);
+
+            return result;
         }
 
         Eigen::VectorXd branch_and_bound(const Eigen::MatrixXd &matrix, const Eigen::VectorXd &target)
@@ -167,20 +201,20 @@ namespace Algorithms
                 return Eigen::VectorXd::Zero(matrix.cols());
             }
             Eigen::VectorXd b = matrix.row(matrix.rows() - 1);
-            Eigen::MatrixXd mat = matrix.block(0, 0, matrix.rows() - 1, matrix.cols());
-            Eigen::VectorXd b_star = Utils::projection(mat, b);
-            Eigen::VectorXd v = Algorithms::CVP::greedy(mat, target);
+            Eigen::MatrixXd B = matrix.block(0, 0, matrix.rows() - 1, matrix.cols());
+            Eigen::VectorXd b_star = Utils::projection(B, b);
+            Eigen::VectorXd v = Algorithms::CVP::greedy(B, target);
 
-            double upper_bound = std::ceil((target - v).norm());
+            double upper_bound = (target - v).norm();
             double x_middle = std::floor(target.dot(b_star) / b_star.dot(b_star));
-            double lower_bound = Utils::projection(mat, target - x_middle * b).norm();
+            double lower_bound = Utils::projection(B, target - x_middle * b).norm();
 
             double x = x_middle;
             double temp_lower_bound = lower_bound;
             while (temp_lower_bound <= upper_bound)
             {
                 x += 1;
-                temp_lower_bound = Utils::projection(mat, target - x * b).norm();
+                temp_lower_bound = Utils::projection(B, target - x * b).norm();
             }
             double x_highest = x;
 
@@ -189,7 +223,7 @@ namespace Algorithms
             while (temp_lower_bound <= upper_bound)
             {
                 x -= 1;
-                temp_lower_bound = Utils::projection(mat, target - x * b).norm();
+                temp_lower_bound = Utils::projection(B, target - x * b).norm();
             }
             double x_lowest = x + 1;
 
@@ -205,11 +239,62 @@ namespace Algorithms
             std::vector<Eigen::VectorXd> v_array;
             for (auto const &elem : x_array)
             {
-                Eigen::VectorXd res = elem * b + Algorithms::CVP::branch_and_bound(mat, target - elem * b);
+                Eigen::VectorXd res = elem * b + Algorithms::CVP::branch_and_bound(B, target - elem * b);
                 v_array.push_back(res);
             }
             return Utils::closest_vector(v_array, target);
         }
+
+        // Eigen::VectorXd branch_and_bound(const Eigen::MatrixXd &matrix, const Eigen::VectorXd &target)
+        // {
+        //     if (matrix.rows() == 0)
+        //     {
+        //         return Eigen::VectorXd::Zero(matrix.cols());
+        //     }
+        //     Eigen::VectorXd b = matrix.row(matrix.rows() - 1);
+        //     Eigen::MatrixXd B = matrix.block(0, 0, matrix.rows() - 1, matrix.cols());
+        //     Eigen::VectorXd b_star = Utils::projection(B, b);
+        //     Eigen::VectorXd v = Algorithms::CVP::greedy(B, target);
+
+        //     double upper_bound = std::ceil((target - v).norm());
+        //     double x_middle = std::floor(target.dot(b_star) / b_star.dot(b_star));
+        //     double lower_bound = Utils::projection(B, target - x_middle * b).norm();
+
+        //     double x = x_middle;
+        //     double temp_lower_bound = lower_bound;
+        //     while (temp_lower_bound <= upper_bound)
+        //     {
+        //         x += 1;
+        //         temp_lower_bound = Utils::projection(B, target - x * b).norm();
+        //     }
+        //     double x_highest = x;
+
+        //     x = x_middle;
+        //     temp_lower_bound = lower_bound;
+        //     while (temp_lower_bound <= upper_bound)
+        //     {
+        //         x -= 1;
+        //         temp_lower_bound = Utils::projection(B, target - x * b).norm();
+        //     }
+        //     double x_lowest = x + 1;
+
+        //     std::vector<int> x_array;
+        //     for (int i =  static_cast<int>(x_lowest); i < x_highest; i++)
+        //     {
+        //         x_array.push_back(i);
+        //     }
+        //     if (x_array.size() == 0)
+        //     {
+        //         x_array.push_back(static_cast<int>(x_middle));
+        //     }
+        //     std::vector<Eigen::VectorXd> v_array;
+        //     for (auto const &elem : x_array)
+        //     {
+        //         Eigen::VectorXd res = elem * b + Algorithms::CVP::branch_and_bound(B, target - elem * b);
+        //         v_array.push_back(res);
+        //     }
+        //     return Utils::closest_vector(v_array, target);
+        // }
     }
     // Computes Gram Schmidt orthogonalization
     // @return Eigen::MatrixXd
@@ -244,7 +329,7 @@ namespace Algorithms
                 projections.noalias() += (inner1 / inner2) * basis_vector;
             }
 
-            Eigen::VectorXd result = vec.cast<double>() - projections;
+            Eigen::VectorXd result = vec - projections;
 
             if (delete_zero_rows)
             {
@@ -266,6 +351,7 @@ namespace Algorithms
         {
             result.col(i) = basis[i];
         }
+
         return result;
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -22,15 +22,15 @@ namespace mp = boost::multiprecision;
 
 int main()
 {
-	Eigen::MatrixXd mat(2, 2);
-	mat << 1.5, 1, 1, 1.5;
+	Eigen::MatrixXd mat(2, 1);
 	mat << 1, 0, 0, 1;
+	//mat << 1, 0, 0, 1;
 	Eigen::VectorXd vec(2);
-	vec << 1.6, 0.6;
+	vec << 1.9, 1;
 	
 	std::cout << mat << "\n\n";
 	std::cout << vec << "\n\n";
-	std::cout << Algorithms::CVP::greedy(mat, vec) << "\n\n";
+	std::cout << "Answer=\n" << Algorithms::CVP::greedy(mat, vec) << "\n\n";
 	//std::cout << Algorithms::CVP::branch_and_bound(mat, vec) << "\n\n";
 
 
